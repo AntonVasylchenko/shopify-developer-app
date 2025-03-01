d433439b31b5fe649029a21fb89dd1f553a4b034
Vasylchenko Anton <87824792+AntonVasylchenko@users.noreply.github.com>
2025-03-01 14:55:06 +0200

    Added logs

--- app/routes/send/route.tsx
+++ app/routes/send/route.tsx
@@ -5,13 +5,20 @@ import { authenticate } from "app/shopify.server";
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const headers = new Headers();
   headers.append("ngrok-skip-browser-warning", "true");
+  console.log("get email",request.body);
+
   return null
 };
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-  const { admin, session } = await authenticate.public.appProxy(request);
   console.log("send email",request.body);
-  return null
+  try {
+    const { admin, session } = await authenticate.public.appProxy(request);
+    return null
+  } catch (error) {
+    console.log(error);
+    return null
+  }
 }
 
 

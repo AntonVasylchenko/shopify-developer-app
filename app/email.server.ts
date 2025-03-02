9a1285ded1b70684564f4945c971edd9bdd0fd4b
Vasylchenko Anton <87824792+AntonVasylchenko@users.noreply.github.com>
2025-03-02 07:48:04 +0200

    start

--- app/email.server.ts
+++ app/email.server.ts
@@ -1,47 +1,47 @@
 import nodemailer from "nodemailer";
 
 export async function sendMail(payload: { email: string; approved: boolean; customer: boolean }) {
-  const { email, approved, customer } = payload;
+//   const { email, approved, customer } = payload;
 
-  const transport = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.GMAIL_PASSWORD,
-    },
-  });
+//   const transport = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 465,
+//     secure: true,
+//     auth: {
+//       user: process.env.EMAIL,
+//       pass: process.env.GMAIL_PASSWORD,
+//     },
+//   });
 
-  let emailHtml = "";
-  let subject = "";
+//   let emailHtml = "";
+//   let subject = "";
 
-  if (approved && customer) { 
-    emailHtml = "<p>Your profile has been verified.</p>"; 
-    subject = "Profile Verification"; 
-  } else if (approved && !customer) { 
-    emailHtml = "<p>Your profile has been verified. To complete the process, you need to register.</p>"; 
-    subject = "Profile Verification - Registration Required"; 
-  } else { 
-    emailHtml = "<p>Your profile has not been verified.</p>"; 
-    subject = "Profile Not Verified"; 
-  }
+//   if (approved && customer) { 
+//     emailHtml = "<p>Your profile has been verified.</p>"; 
+//     subject = "Profile Verification"; 
+//   } else if (approved && !customer) { 
+//     emailHtml = "<p>Your profile has been verified. To complete the process, you need to register.</p>"; 
+//     subject = "Profile Verification - Registration Required"; 
+//   } else { 
+//     emailHtml = "<p>Your profile has not been verified.</p>"; 
+//     subject = "Profile Not Verified"; 
+//   }
   
 
-  const mailOptions = {
-    from: process.env.EMAIL_SENDER,
-    to: email,
-    subject,
-    html: emailHtml,
-  };
+//   const mailOptions = {
+//     from: process.env.EMAIL_SENDER,
+//     to: email,
+//     subject,
+//     html: emailHtml,
+//   };
 
-  try {
-    const info = await transport.sendMail(mailOptions);
-    console.log("Email was sent to " + email, info.response);
+//   try {
+//     const info = await transport.sendMail(mailOptions);
+//     console.log("Email was sent to " + email, info.response);
 
-    return { success: true, message: "Email was sent" };
-  } catch (error) {
-    console.error("Error sending email:", error);
-    return { success: false, message: "Failed to send email", error };
-  }
+//     return { success: true, message: "Email was sent" };
+//   } catch (error) {
+//     console.error("Error sending email:", error);
+//     return { success: false, message: "Failed to send email", error };
+//   }
 }
